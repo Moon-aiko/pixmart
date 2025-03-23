e53d3e34b4a196ce70bff1661caf471a73ca3236
Andrew Gurylev <andrew@gurylev.com>
2025-03-23 15:24:16 +0200

    fix(build): remove delay from watching over non-styles there's delay is useless, but disturbing during changing pages

--- gulpfile.js
+++ gulpfile.js
@@ -244,9 +244,9 @@ export const build = series(clean, parallel(styles, images, sprite, markup))
 
 const watchFiles = (cb) => {
   watch(paths.dev.scss, { delay: 1000 }, styles)
-  watch(paths.dev.images, { delay: 1000 }, images)
-  watch(paths.dev.svg, { delay: 1000 }, sprite)
-  watch(paths.dev.views, { delay: 1000 }, markup)
+  watch(paths.dev.images, images)
+  watch(paths.dev.svg, sprite)
+  watch(paths.dev.views, markup)
   cb()
 }
 export { watchFiles as watch }
