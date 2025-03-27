aa9d107c5eb8cd81459c738a32c7fa11db94e7d2
Andrew Gurylev <andrew@gurylev.com>
2025-03-27 10:45:24 +0200

    fix(build): add svg, webp, avif optimisation

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,11 @@ import { parseArgs } from 'node:util'
 import { src, series, parallel, dest, watch, lastRun } from 'gulp'
 import { deleteAsync } from 'del'
 import gulpif from 'gulp-if'
-import filesize from 'gulp-filesize'
+import size from 'gulp-size'
 import rename from 'gulp-rename'
 import data from 'gulp-data'
 import sharpResponsive from 'gulp-sharp-responsive'
+import svgo from 'gulp-svgo'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 import postcss from 'gulp-postcss'
@@ -37,14 +38,15 @@ const paths = {
     svg: dev+'images/sprite/*.svg',
     views: dev+'templates/**/*.{json,njk,html}',
     pages: dev+'templates/pages/*/*.{njk,html}',
-    images: dev+'images/static/**/*.{jpg,jpeg,png,svg}',
+    modernImages: dev+'images/static/**/*.{webp,avif}',
+    svgStatic: dev+'images/static/**/*.svg',
+    images: dev+'images/static/**/*.{jpg,jpeg,png}',
   },
   dist: {
       pages: dist,
       styles: dist+'css',
       scripts: dist+'js',
       images: dist+'img',
-      svg: dist+'img'
   }
 };
 
@@ -52,6 +54,9 @@ const config = {
   devPort: 8080,
   uiPort: 7171
 };
+const sizeOptions = {
+  showFiles: true,
+}
 const sass = gulpSass(dartSass)
 
 const { values:args } = parseArgs({
@@ -175,11 +180,59 @@ export const styles = () =>
       })
     ], { syntax: postcssScss }))
     .pipe(dest(paths.dist.styles, { sourcemaps: true }))
-    .pipe(gulpif(args.debug, filesize()))
+    .pipe(gulpif(args.debug, size(sizeOptions)))
     .pipe(bs.stream())
 
-export const images = () =>
+export const copyImages = () =>
+  src(paths.dev.modernImages, { since: lastRun(copyImages), encoding: false })
+    .pipe(gulpif(args.debug, size({...sizeOptions, title: 'Unotimised (modern raster): '})))
+    .pipe(sharpResponsive({
+      formats: [
+        {
+          format: 'webp',
+          rename: { suffix: "-2x" },
+          webpOptions: { lossless: true }
+        },
+        {
+          format: 'avif',
+          rename: { suffix: "-2x" },
+          avifOptions: { lossless: true }
+        },
+        {
+          format: 'webp',
+          rename: { suffix: "-1x" },
+          webpOptions: { lossless: true },
+          width: (metadata) => Math.round(metadata.width * 0.5),
+        },
+        {
+          format: 'avif',
+          rename: { suffix: "-1x" },
+          avifOptions: { lossless: true },
+          width: (metadata) => Math.round(metadata.width * 0.5),
+        },
+      ]
+    }))
+    .pipe(dest(paths.dist.images))
+    .pipe(gulpif(args.debug, size({...sizeOptions, title: 'Optimized (modern raster): '})))
+    .pipe(bs.stream())
+
+export const optimizeVectorImages = () =>
+  src(paths.dev.svgStatic)
+    .pipe(gulpif(args.debug, size({...sizeOptions, title: 'Unotimised (vector): '})))
+    .pipe(svgo({
+      plugins: [{
+        cleanupNumericValues: {
+          floatPrecision: 0
+        }
+      }]
+    }))
+    .pipe(dest(paths.dist.images))
+    .pipe(gulpif(args.debug, size({...sizeOptions, title: 'Optimised (vector): '})))
+    .pipe(bs.stream())
+
+export const optimizeRasterImages = () =>
   src(paths.dev.images, { since: lastRun(images), encoding: false })
+    .pipe(gulpif(args.debug, size({...sizeOptions, title: 'Unotimised (raster): '})))
     .pipe(sharpResponsive({
       formats: [
         { format: 'webp', rename: { suffix: "-2x" } },
@@ -189,7 +242,10 @@ export const images = () =>
       ]
     }))
     .pipe(dest(paths.dist.images))
+    .pipe(gulpif(args.debug, size({...sizeOptions, title: 'Optimised (raster): '})))
     .pipe(bs.stream())
+
+export const images = parallel(copyImages, optimizeRasterImages, optimizeVectorImages)
 
 export const sprite = () =>
   src(paths.dev.svg)
@@ -197,13 +253,14 @@ export const sprite = () =>
       mode: {
         symbol: {
           dest: '.',
-          sprite: join(paths.dist.svg, 'sprite.svg'),
+          sprite: join(paths.dist.images, 'sprite.svg'),
           dimensions: false,
           bust: false
         }
       }
     }))
     .pipe(dest('.'))
+    .pipe(gulpif(args.debug, size({...sizeOptions, title: 'Optimised: '})))
     .pipe(bs.stream())
 
 export const markup = () =>
@@ -219,7 +276,7 @@ export const markup = () =>
     .pipe(htmlLint.format(htmllintReporter))
     .pipe(rename({ dirname: '' }))
     .pipe(dest(paths.dist.pages))
-    .pipe(gulpif(args.debug, filesize()))
+    .pipe(gulpif(args.debug, size(sizeOptions)))
     .pipe(bs.stream())
 
 export const liveReload = () =>
